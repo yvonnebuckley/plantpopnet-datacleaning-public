edf892d5b5383b0bccbd4f0ec95017b60ad5b162
Yvonne Buckley <buckleyy@tcd.ie>
2022-05-04 16:59:59 +0100

    Delete Y0_analysis_scripts directory

--- Y0_analysis_scripts/YB_Y0_script.R
+++
@@ -1,6 +0,0 @@
-## YB working with Y0
-## 10/12/19
-setwd("~/OneDrive - TCDUD.onmicrosoft.com/Yvonne OneDrive/plantpopnet/Data Cleaning/YB_Plantpopnet-Data-Cleaning.Rproj/Master")
-dat0 <- read.csv("PLANTPOPNET_Y0_draftV1.1_2019-11-08_.csv", header = TRUE)
-
-dim(dat0)
